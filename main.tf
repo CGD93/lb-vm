2c6ef43535a231b2955477bcf048720c9b397fb9
ChoiGwangDeok <111724209+CGD93@users.noreply.github.com>
2023-05-24 10:47:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,6 +78,28 @@ resource "azurerm_virtual_machine" "VM" {
     tags = var.tag
 }
 
+resource "azurerm_network_security_group" "NSG" {
+  name = var.NSG-name
+  location = var.RG-location
+  resource_group_name = var.RG-name
+  tags = var.tag
+  security_rule {
+    name                       = var.security_rule_name
+    priority                   = var.security_rule_priority
+    direction                  = var.security_rule_direction
+    access                     = var.security_rule_access
+    protocol                   = var.security_rule_protocol
+    source_port_range          = var.security_rule_source_port_range
+    destination_port_range     = var.security_rule_destination_port_range
+    source_address_prefix      = var.security_rule_source_address_prefix
+    destination_address_prefix = var.security_rule_destination_address_prefix
+  }
+}
+resource "azurerm_network_interface_security_group_association" "join" {
+  network_interface_id = azurerm_network_interface.NIC_Static.id
+  network_security_group_id = azurerm_network_security_group.NSG.id
+}
+
 resource "azurerm_public_ip" "CGD-LB-Public" {
   name                = "CDG_Pu-LB"
   location            = azurerm_resource_group.RG.location
